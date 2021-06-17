b6ada81a9e933acd396698dd6b25dbd435ac2e82
Jason Cope <jason.cope@noaa.gov>
2021-06-17 09:26:08 -0700

    Small text change

--- ui.R
+++ ui.R
@@ -8,8 +8,8 @@ library(shiny)
     h5(p(em("Uncertainty can also be added to the estimates by chosing a coefficient of variation (CV) value and error distribution type."))),
     h5(p(em("Downloaded R objects (.DMP) can be loaded directly into R or, if using the R terminal, drag and drop in."))),
     br(),
-    h4(p("References for each method can be found",tags$a(href="javascript:window.open('References_M.html', '_blank','width=600,height=400')", "here"))),
     h4(p("Do you have any suggested methods to add? Please submit an issue with the recommendation" ,tags$a(href="https://github.com/shcaba/Natural-Mortality-Tool/issues", "here"))),
+    h4(p("References for each included method can be found",tags$a(href="javascript:window.open('References_M.html', '_blank','width=600,height=400')", "here"))),
     
     sidebarLayout(
     sidebarPanel(
